f90eb22ae8e4c61b5d6241a75959818a9e10d047
Jegern <severuanov2001@mail.ru>
2022-05-18 11:39:10 +0500

    Поправлен интерфейс редактирования элективов. Добавлена возможность достать выбранный электив.

--- ViewModels/ElectiveEditingViewModel.cs
+++ ViewModels/ElectiveEditingViewModel.cs
@@ -2,6 +2,7 @@
 using Elective_Choice.Models;
 using Elective_Choice.ViewModels.Base;
 using Elective_Choice.ViewModels.Store;
+using Laboratory_work_1.Commands.Base;
 using Npgsql;
 
 namespace Elective_Choice.ViewModels;
@@ -9,6 +10,13 @@ namespace Elective_Choice.ViewModels;
 public class ElectiveEditingViewModel : ViewModel
 {
     public ObservableCollection<Elective>? Electives { get; }
+    private Elective? _selectedElective;
+    
+    public Elective? SelectedElective
+    {
+        get => _selectedElective;
+        set => Set(ref _selectedElective, value);
+    }
     
     public ElectiveEditingViewModel()
     {
@@ -17,6 +25,10 @@ public class ElectiveEditingViewModel : ViewModel
     public ElectiveEditingViewModel(ViewModelStore? store) : base(store)
     {
         Electives = GetCurrentElectives();
+
+        OpenElectiveCommand = new Command(
+            OpenElectiveCommand_OnExecuted,
+            OpenElectiveCommand_CanExecute);
     }
 
     private ObservableCollection<Elective> GetCurrentElectives()
@@ -34,4 +46,21 @@ public class ElectiveEditingViewModel : ViewModel
 
         return electives;
     }
+
+    #region Commands
+
+    #region OpenElectiveCommand
+
+    public Command? OpenElectiveCommand { get; }
+
+    private bool OpenElectiveCommand_CanExecute(object? parameter) => SelectedElective is not null;
+
+    private void OpenElectiveCommand_OnExecuted(object? parameter)
+    {
+        
+    }
+
+    #endregion
+
+    #endregion
 }
