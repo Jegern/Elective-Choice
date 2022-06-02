b13b321f84f17519028bec70f691c6d700c8c6f4
Jegern <severuanov2001@mail.ru>
2022-06-03 00:14:22 +0500

    Добавлена промежуточная версия настроек алгоритма

---
+++ Elective Choice/ViewModels/Admin/AlgorithmSettingsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Elective_Choice.Infrastructure.EventSource;
+using Elective_Choice.ViewModels.Base;
+
+namespace Elective_Choice.ViewModels.Admin;
+
+public class AlgorithmSettingsViewModel : ViewModel
+{
+    #region Fields
+
+    #endregion
+    
+    #region Constructor
+
+    public AlgorithmSettingsViewModel()
+    {
+        
+    }
+    
+    public AlgorithmSettingsViewModel(EventSource source) : base(source)
+    {
+    }
+
+    #endregion
+}
---
+++ Elective Choice/Views/Admin/AlgorithmSettings.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Elective_Choice.Infrastructure.EventSource;
+using Elective_Choice.ViewModels.Admin;
+
+namespace Elective_Choice.Views.Admin;
+
+public partial class AlgorithmSettings
+{
+    public AlgorithmSettings(EventSource source)
+    {
+        InitializeComponent();
+        DataContext = new AlgorithmSettingsViewModel(source);
+    }
+}
